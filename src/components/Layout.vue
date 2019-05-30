96d5d60d0cdbadf96a56664b87450246d2f84873
yszs <362409100@qq.com>
2019-05-30 17:25:41 +0800

    first commit

---
+++ src/components/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="container">
+    <section class="menu">
+      <menus></menus>
+    </section>
+    <section class="content-container"></section>
+  </section>
+</template>
+
+<script>
+import menus from "./Menus";
+
+export default {
+  components: {
+    menus
+  }
+};
+</script>
+
+<style lang="less">
+@import "../common/style/layouts.less";
+</style>
+    
